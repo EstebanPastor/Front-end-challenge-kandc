a476e82449b5e6e8396b7be9b9e76765bd6442a4
EstebanPastor <estebanandrespastor@gmail.com>
2023-09-26 13:51:32 -0300

    feat: :alien: succesfully fetched the images in the api and now are showable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
-import Image from 'next/image'
+import api from "@/api";
 
-export default function Home() {
+export default async function Home() {
+  const caseStudies = await api.caseStudy.list();
   return (
-    <main>
-      
-      
-          
-    </main>
-  )
+    <article>
+      {caseStudies.map((caseStudy) => (
+        <hgroup key={caseStudy.id}>
+          <img
+            src={caseStudy.hero_image}
+            height={256}
+            className="w-full object-cover"
+            alt="case-study-image"
+          />
+          <h2>{caseStudy.title}</h2>
+          <p>{caseStudy.teaser}</p>
+        </hgroup>
+      ))}
+    </article>
+  );
 }
