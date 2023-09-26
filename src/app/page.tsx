bd563ab452323954ec2ff4f500de8a7a12414298
EstebanPastor <estebanandrespastor@gmail.com>
2023-09-26 14:01:53 -0300

    feat: :art: add css style to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,25 @@
 import api from "@/api";
+import Link from "next/link";
 
 export default async function Home() {
   const caseStudies = await api.caseStudy.list();
   return (
-    <article>
+    <article className="grid gap-8 grid-cols-[repeat(auto-fill,minmax(320px,1fr))]">
       {caseStudies.map((caseStudy) => (
-        <hgroup key={caseStudy.id}>
+      <Link key={caseStudy.id} href={`${caseStudy.id}`}>
+        <hgroup  className="grid gap-2">
           <img
             src={caseStudy.hero_image}
             height={256}
             className="w-full object-cover"
             alt="case-study-image"
           />
-          <h2>{caseStudy.title}</h2>
-          <p>{caseStudy.teaser}</p>
+          <div>
+            <h2 className="font-bold text-2xl">{caseStudy.title}</h2>
+            <p className="opacity-80">{caseStudy.teaser}</p>
+          </div>
         </hgroup>
+      </Link>
       ))}
     </article>
   );
